9a142c018d553c325eca3242d1aa77f2ce323456
Marcio Costa <marcioc424@gmail.com>
2020-09-26 17:29:44 -0300

    Desafio 1 - Exercício 3 - OK

--- desafio-1/exercicio-03.js
+++ desafio-1/exercicio-03.js
@@ -5,9 +5,11 @@
  * @param texto: texto a ser avaliado;
  * @return: texto com as letras maiúsculas na frente e posteriormente as letras minúsculas;
  */
-function moverLetrasMaiusculasParaAFrente(texto) {
-  
-  //TODO: Implemente sua lógica aqui
+function moverLetrasMaiusculasParaAFrente() {
+  let inputValue = document.getElementById('entrada-exercicio-3').value
+  inputValue = [...inputValue];
+  const sorted1 = inputValue.sort(letter =>  /[A-Z]/.test(letter) ? -1 : 0).join('');
 
-  return "Não Implementado";
+
+  return sorted1;
 }
--- desafio-1/exercicio-04.js
+++ desafio-1/exercicio-04.js
@@ -8,8 +8,27 @@
  * @return: Vetor com os números pares na frente e os números ímpares posteriormente;
  */
 function moverNumerosParesParaAFrenteDoVetor(vetor) {
+  let inputValue = document.getElementById('entrada-exercicio-4').value
+  
+  let array = inputValue.split(",");
 
-  //TODO: Implemente sua lógica aqui
+  return sortArray(array);
+}
 
-  return "Não Implementado";
+function sortArray(array){    
+  let arrayLength = array.length;
+  for(let i = 0; i < arrayLength; i++){
+
+      for(let j = 0; j < arrayLength; j++){
+
+          if(array[j] > array[i+1]){
+              let valueTemp = array[j];
+              array[j] = array[j+1];
+              array[j+1] = valueTemp;
+          }
+
+      }
+
+  }
+  return array;
 }
