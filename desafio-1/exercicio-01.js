d9bc6278ae6b82c83c4c955f0058d296a0a9dfa2
Marcio Costa <marcioc424@gmail.com>
2020-09-26 17:07:39 -0300

    Desafio 1 - Exercício 1 - OK

--- desafio-1/exercicio-01.js
+++ desafio-1/exercicio-01.js
@@ -6,8 +6,18 @@
  * @return: número de vogais existentes;
  */
 function calcularAQuantidadeDeVogais(texto) {
-  
+  let inputValue = document.getElementById('entrada-exercicio-1').value.toLowerCase().normalize("NFD");
+  let length = 0;
+
+  for(let i = 0; i < inputValue.length; i++){
+    let letter = inputValue[i];
+    if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+      length++
+
+  }
+
+  console.log(inputValue, length);
   //TODO: Implemente sua lógica aqui
   
-  return "Não Implementado";
+  return length;
 }
